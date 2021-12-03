e5ad8e376a0889f152c1f31a5adc3651b62693fd
Vinaybannur <vinaybannur143@gmail.com>
2021-12-03 11:02:50 +0530

    login

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,11 @@
 import express from "express";
 import {createUser, genPassword, getUserByName} from "../helper.js";
+import bcrypt from 'bcrypt';
+
+
 const router = express.Router();
+
+//signup
 
 router.post("/signup",async(request,response)=>{
    
@@ -27,6 +32,32 @@ router.post("/signup",async(request,response)=>{
     const hashedPassword = await genPassword(password);
     const result = await createUser({username,password:hashedPassword});
     response.send(result);
+})
+
+//login
+
+router.post("/login",async(request,response)=>{
+   
+    const {username, password} = request.body;
+    
+    const userFromDB = await getUserByName(username)
+    //check for username
+    if(!userFromDB){
+      response.status(400).send({message:"invalid credentials"});
+      return;
+    }
+
+    //if password is match
+    const storedPassword = userFromDB.password;
+
+    const isPasswordMatch = await bcrypt.compare(password,storedPassword);
+    // console.log(isPasswordMatch);
+    
+    if(isPasswordMatch){
+        response.send({message:"Successful login"})
+    }else{
+        response.status(400).send({message:"invalid credentials"});
+    }
 })
 
 export const usersRouter = router;
